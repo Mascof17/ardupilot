071102996cb471da43cdd4af34ac5d326e3ac249
Zéphyr de Rivière <zephyr.de_riviere@etu.minesparis.psl.eu>
2025-04-04 16:52:00 +0200

    test3

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -178,14 +178,14 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
 
     case SUB_FRAME_CUSTOM:
         _frame_class_string = "Custom";
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,    -(std::sqrt(2)/std::sqrt(3))*f,  (std::sqrt(2)/std::sqrt(3))*f,   -(std::sqrt(2)/std::sqrt(3))*f,     -(std::sqrt(2)/std::sqrt(3))*f,    0,    (std::sqrt(2)/std::sqrt(3))*f, 1);
-        add_motor_raw_6dof(AP_MOTORS_MOT_2,    -(std::sqrt(2)/std::sqrt(3))*f,  (std::sqrt(2)/std::sqrt(3))*f,    (std::sqrt(2)/std::sqrt(3))*f,   -(std::sqrt(2)/std::sqrt(3))*f,   0,    -(std::sqrt(2)/std::sqrt(3))*f,  2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,    -(std::sqrt(2)/std::sqrt(3)),  (std::sqrt(2)/std::sqrt(3)),   -(std::sqrt(2)/std::sqrt(3)),     -(std::sqrt(2)/std::sqrt(3)),    0,    (std::sqrt(2)/std::sqrt(3)), 1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,    -(std::sqrt(2)/std::sqrt(3)),  (std::sqrt(2)/std::sqrt(3)),    (std::sqrt(2)/std::sqrt(3)),   -(std::sqrt(2)/std::sqrt(3)),   0,    -(std::sqrt(2)/std::sqrt(3)),  2);
         add_motor_raw_6dof(AP_MOTORS_MOT_3,    -1.0f,    -1.0f,    1.0f,   -1.0f,     -1.0f,   -1.0f,     3);
         add_motor_raw_6dof(AP_MOTORS_MOT_4,    1.0f,   1.0f,      1.0f,  1.0f,     1.0f,   1.0f,      4);
         add_motor_raw_6dof(AP_MOTORS_MOT_5,    1.0f,   -1.0f,     -1.0f,     -1.0f,    -1.0f,   1.0f,     5);
         add_motor_raw_6dof(AP_MOTORS_MOT_6,    -1.0f,    1.0f,     -1.0f,  1.0f,      -1.0f,  1.0f,     6);
-        add_motor_raw_6dof(AP_MOTORS_MOT_7,    (std::sqrt(2)/std::sqrt(3))*f,   (std::sqrt(2)/std::sqrt(3))*f,   (std::sqrt(2)/std::sqrt(3))*f,    -(std::sqrt(2)/std::sqrt(3))*f,    0,    -(std::sqrt(2)/std::sqrt(3))*f,  7);
-        add_motor_raw_6dof(AP_MOTORS_MOT_8,    (std::sqrt(2)/std::sqrt(3))*f,  (std::sqrt(2)/std::sqrt(3))*f,  -(std::sqrt(2)/std::sqrt(3))*f,    -(std::sqrt(2)/std::sqrt(3))*f,    0,   (std::sqrt(2)/std::sqrt(3))*f,    8);
+        add_motor_raw_6dof(AP_MOTORS_MOT_7,    (std::sqrt(2)/std::sqrt(3)),   (std::sqrt(2)/std::sqrt(3)),   (std::sqrt(2)/std::sqrt(3)),    -(std::sqrt(2)/std::sqrt(3)),    0,    -(std::sqrt(2)/std::sqrt(3)),  7);
+        add_motor_raw_6dof(AP_MOTORS_MOT_8,    (std::sqrt(2)/std::sqrt(3)),  (std::sqrt(2)/std::sqrt(3)),  -(std::sqrt(2)/std::sqrt(3)),    -(std::sqrt(2)/std::sqrt(3)),    0,   (std::sqrt(2)/std::sqrt(3)),    8);
         break;
 
 
